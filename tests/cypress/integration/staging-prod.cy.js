48d26de2b93fb8b79e25675d05962017b21538f5
Evan Mullins <evanrm@gmail.com>
2024-03-15 11:53:59 -0400

    break into multiple test files for better isolation

--- tests/cypress/integration/staging-prod.cy.js
+++ tests/cypress/integration/staging-prod.cy.js
@@ -4,10 +4,8 @@ const stagingCloneFixture = require( '../fixtures/stagingClone.json' );
 const stagingDeleteFixture = require( '../fixtures/stagingDelete.json' );
 const stagingCreateFixture = require( '../fixtures/stagingCreate.json' );
 const stagingSwitchFixture = require( '../fixtures/stagingSwitch.json' );
-const stagingStagingFixture = require( '../fixtures/stagingStaging.json' );
-const stagingDeployFixture = require( '../fixtures/stagingDeploy.json' );
 
-describe( 'Staging Page', function () {
+describe( 'Staging Page - Production Environment', function () {
 	const appClass = '.' + Cypress.env( 'appId' );
 
 	before( () => {
@@ -235,87 +233,5 @@ describe( 'Staging Page', function () {
 			.should( 'be.visible' );
 
 		// actual reload cancelled by fixture containing a load_page value of `#`
-	} );
-} );
-
-describe( 'Staging Page - Staging environmant', function () {
-	const appClass = '.' + Cypress.env( 'appId' );
-
-	before( () => {
-		cy.intercept(
-			{
-				method: 'GET',
-				url: /newfold-staging(\/|%2F)v1(\/|%2F)staging/,
-			},
-			stagingStagingFixture
-		);
-		cy.visit(
-			'/wp-admin/admin.php?page=' +
-				Cypress.env( 'pluginId' ) +
-				'#/staging'
-		);
-	} );
-
-	it( 'Displays staging environemnt properly', () => {
-		cy.get( '#newfold-production-toggle' ).should( 'not.be.checked' );
-		cy.get( '.newfold-staging-prod' )
-			.contains( 'h3', 'Production Site' )
-			.should( 'be.visible' );
-		cy.get( '.newfold-staging-prod' )
-			.contains(
-				'label[for="newfold-production-toggle"]',
-				'Not currently editing'
-			)
-			.should( 'be.visible' );
-
-		cy.get( '#newfold-staging-toggle' ).should( 'be.checked' );
-		cy.get( '.newfold-staging-staging' )
-			.contains( 'h3', 'Staging Site' )
-			.should( 'be.visible' );
-		cy.get( '.newfold-staging-staging' )
-			.contains(
-				'label[for="newfold-staging-toggle"]',
-				'Currently editing'
-			)
-			.should( 'be.visible' );
-
-		cy.get( '#staging-clone-button' ).should( 'be.disabled' );
-		cy.get( '#staging-delete-button' ).should( 'be.disabled' );
-		cy.get( '#staging-deploy-button' ).should( 'not.be.disabled' );
-	} );
-
-	it( 'Deploy Works', () => {
-		cy.intercept(
-			{
-				method: 'POST',
-				url: /newfold-staging(\/|%2F)v1(\/|%2F)staging(\/|%2F)deploy/,
-			},
-			{
-				body: stagingDeployFixture,
-				delay: 500,
-			}
-		).as( 'stagingDeploy' );
-
-		cy.get( '#staging-deploy-button' ).click();
-		cy.get( '.nfd-modal' )
-			.contains( 'h1', 'Confirm Deployment' )
-			.should( 'be.visible' );
-		cy.get( '.nfd-modal .nfd-button--error' )
-			.contains( 'Cancel' )
-			.should( 'be.visible' );
-		cy.get( '.nfd-modal .nfd-button--primary' )
-			.contains( 'Deploy' )
-			.should( 'be.visible' )
-			.click();
-
-		cy.get( '.nfd-notifications' )
-			.contains( 'p', 'Working...' )
-			.should( 'be.visible' );
-
-		cy.wait( '@stagingDeploy' );
-
-		cy.get( '.nfd-notifications' )
-			.contains( 'p', 'Deployed' )
-			.should( 'be.visible' );
 	} );
 } );
