a1921af56499fcb8422d68b2d82343cbff92d7a1
Evan Mullins <evanrm@gmail.com>
2024-05-13 15:21:42 -0400

    use feature hooks class for managing the hooks

--- bootstrap.php
+++ bootstrap.php
@@ -1,30 +1,5 @@
 <?php
 
-use function NewfoldLabs\WP\Context\getContext;
-use function NewfoldLabs\WP\Module\Features\getFeature;
+namespace NewfoldLabs\WP\Module\Staging;
 
-if ( function_exists( 'add_action' ) ) {
-
-	// update as needed based on context
-	add_filter(
-		'newfold/features/filter/isEnabled/staging',
-		function($value) {
-			if ( 'atomic' === getContext( 'platform' ) ) {
-				$value = false;
-			}
-			return $value;
-		}
-	);
-
-	add_action(
-		'after_setup_theme',
-		function () {
-			if ( 'atomic' === getContext( 'platform' ) ) {
-				$stagingFeature = getFeature('staging');
-				if ( $stagingFeature ) {
-					$stagingFeature->disable();
-				}
-			}
-		}
-	);
-}
+new StagingFeatureHooks();
