1702b13315ac547305abb585fe2d497b20738705
Evan Mullins <evanrm@gmail.com>
2024-05-21 16:46:09 -0400

    check function_exists first

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,11 @@
 
 namespace NewfoldLabs\WP\Module\Staging;
 
-add_filter( 
-    'newfold/features/filter/register', 
-    function( $features ) { 
-        return array_merge( $features, array( StagingFeature::class ) );
-    }
-);
+if ( function_exists( 'add_filter' ) ) {
+    add_filter( 
+        'newfold/features/filter/register', 
+        function( $features ) { 
+            return array_merge( $features, array( StagingFeature::class ) );
+        }
+    );
+}
