ac0c9fdad90fb26622b6683b2c5860469b03ab46
krsomayagi <kr.somayagi@newfold.com>
2024-09-03 12:33:25 +0530

    PS0-1962 -A 20-second wait time has been added to load mock data.

--- tests/cypress/integration/staging-prod.cy.js
+++ tests/cypress/integration/staging-prod.cy.js
@@ -4,6 +4,7 @@ const stagingCloneFixture = require( '../fixtures/stagingClone.json' );
 const stagingDeleteFixture = require( '../fixtures/stagingDelete.json' );
 const stagingCreateFixture = require( '../fixtures/stagingCreate.json' );
 const stagingSwitchFixture = require( '../fixtures/stagingSwitch.json' );
+const customCommandTimeout = 20000;
 
 describe( 'Staging Page - Production Environment', function () {
 	const appClass = '.' + Cypress.env( 'appId' );
@@ -15,7 +16,7 @@ describe( 'Staging Page - Production Environment', function () {
 				url: /newfold-staging(\/|%2F)v1(\/|%2F)staging/,
 			},
 			stagingInitFixture
-		);
+		).as('mock-staging-data');
 		cy.visit(
 			'/wp-admin/admin.php?page=' +
 				Cypress.env( 'pluginId' ) +
@@ -27,6 +28,10 @@ describe( 'Staging Page - Production Environment', function () {
 		cy.injectAxe();
 		cy.wait( 500 );
 		cy.checkA11y( appClass + '-app-body' );
+		cy.wait('@mock-staging-data', { timeout: customCommandTimeout } )
+			.then( ( interception ) => {
+				expect( interception.response.statusCode ).to.eq( 200 );
+			});
 	} );
 
 	it( 'Displays in Production Environment Properly', () => {
