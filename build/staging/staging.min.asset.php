274aff858069b2fb37de33e52ae294301afdc9f3
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-04-16 16:20:32 +0200

    Fix: style issue

--- build/staging/staging.min.asset.php
+++ build/staging/staging.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '68af84c38c1a7222ce3c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b9a69ae3e2f278158e04');
