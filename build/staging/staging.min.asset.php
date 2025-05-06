4f753b5c1cdbc6a842afe507011aaae7ab771767
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-05-06 12:34:17 +0200

    Fix: phpcs

--- build/staging/staging.min.asset.php
+++ build/staging/staging.min.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '591c5ad6ba45f7067126');
+<?php return array(
+	'dependencies' => array( 'lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n' ),
+	'version'      => '591c5ad6ba45f7067126',
+);
