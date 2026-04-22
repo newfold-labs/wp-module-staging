1ec6223dd14e801225c3b14da90337594e6a7b8b
Giuseppe Arcifa <giuseppe_arcifa@libero.it>
2026-04-22 18:26:08 +0200

    Fixed loader opacity

--- build/staging/bundle.asset.php
+++ build/staging/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '34f9c368c05cffa4eba6');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '56e477cfb98f9b165368');
